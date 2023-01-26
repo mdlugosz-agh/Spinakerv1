1752bf07485933bd513227f81b64a5824b91cc22
Marek Długosz <81410184+mdlugosz-agh@users.noreply.github.com>
2023-01-26 12:11:15 +0100

    Docker image/container for tensorflow

---
+++ scripts/tf_jupyter_start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# -u $(id -u):$(id -g)
+
+if [ -n "$1" ]
+then
+    PORT=${1}
+else
+    PORT=8888
+fi
+
+echo "Run tensorflow-gpu-jupyter container on port: ${PORT}"
+
+docker run --runtime=nvidia -it --rm \
+    -v ${PWD}:/tf/SpinakerV1 \
+    -v ${PWD}/../SpinakerV1Data:/tf/SpinakerV1Data \
+    --privileged \
+    --network host \
+    -p 8888:8888 \
+    spinaker_tensorflow:gpu-jupyter
--- scripts/tf_local_start.sh
+++ scripts/tf_local_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # -u $(id -u):$(id -g)
 
-docker run --gpus all -it --rm \
+docker run --runtime=nvidia -it --rm \
     -v ${PWD}:/code/SpinakerV1 \
     -v ${PWD}/../SpinakerV1Data:/code/SpinakerV1Data \
     -v /tmp/.X11-unix:/tmp/.X11-unix \

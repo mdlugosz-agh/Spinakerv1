73d80340e434f901ce7cc088a019f9a81c844353
Marek Długosz <81410184+mdlugosz-agh@users.noreply.github.com>
2023-05-19 10:08:33 +0200

    PID controller upgrade

--- packages/controllers/src/pid_controller_node.py
+++ packages/controllers/src/pid_controller_node.py
@@ -3,14 +3,14 @@ import rospy
 from std_msgs.msg import Float32
 from duckietown_msgs.msg import Twist2DStamped
 from numpy import rad2deg as rad2deg
+from duckietown.dtros import DTROS, DTParam, NodeType, ParamType
 
 class PIDController:
+
     def __init__(self):
-    
-        # controller coefficients
-        self.Kp = 1.0
-        self.Ki = 0.0
-        self.Kd = 0.0
+        self.Kp = None
+        self.Ki = None
+        self.Kd = None
 
     def run(self, v_0, theta_ref, theta_hat, prev_e, prev_int, delta_t):
         """
@@ -43,51 +43,68 @@ class PIDController:
         # PID controller for omega
         omega = self.Kp*e + self.Ki*e_int + self.Kd*e_der
         
-        rospy.loginfo(f"Delta time : {delta_t} \nE : {rad2deg(e)} \nE int : {e_int} \nPrev e : {prev_e} \nomega : {omega} \nTheta hat: {rad2deg(theta_hat)}")
-        
         return v_0, omega, e, e_int
+    
+    def set_param(self, params)->None :
+        self.Kp = params['Kp']
+        self.Ki = params['Ki']
+        self.Kd = params['Kd']
     
 ###################################################################################
 
-class WrapperController:
-    def __init__(self):
+class WrapperController(DTROS):
+
+    def __init__(self, node_name):
+        super(WrapperController, self).__init__(
+            node_name=node_name,
+            node_type=NodeType.CONTROL
+        )
+        # Read controller parameters
+        self.controler_param = DTParam('~pid_param', param_type=ParamType.DICT)
+
+        # Set maximum angular speed
+        self.omega_max = DTParam('~omega_max', param_type=ParamType.FLOAT)
         
+        # Set maximum linear speed
+        self.v_max = DTParam('~v_max', param_type=ParamType.FLOAT)
+
+        # Set maximum linear speed
+        self.delta_t = DTParam('~delta_t', param_type=ParamType.FLOAT)
+
+        # Controller variables
+        self.prev_e = 0.0
+        self.prev_int = 0.0
+
+        # Create controller
         self.controller = PIDController()
 
         # Subscribe to error topic
         self.error_sub = rospy.Subscriber('~error', Float32, self.callback, queue_size=1)
         
+        # Message to publish
+        self.twist = Twist2DStamped()
+
         # Publishers
         self.control_pub = rospy.Publisher('~car_cmd', Twist2DStamped, queue_size=1)
 
-        self.twist = Twist2DStamped()
-        
-        self.rate = rospy.Rate(30)
-
-        # Maximum angular and linear speed
-        self.omega_max = rospy.get_param('~omega_max', 8.0)
-        self.v_max = rospy.get_param('~v_max', 0.4)
-
-        self.prev_e = 0.0
-        self.prev_int = 0.0
-
-        # Clean up before stop
-        rospy.on_shutdown(self.cleanup)
-
     def callback(self, msg) -> None:
         try:
+            
+            # Set controller paramters (can by changed during tune process)
+            self.controler_param.force_update()
+            self.controller.set_param(self.controler_param.value)
 
             # Compute controll
             self.twist.v, self.twist.omega, self.prev_e, self.prev_int = self.controller.run(
-                    v_0         = self.v_max, 
+                    v_0         = self.v_max.value, 
                     theta_ref   = 0.0,
                     theta_hat   = msg.data,  
                     prev_e      = self.prev_e, 
                     prev_int    = self.prev_int, 
-                    delta_t     = float(1/30))
+                    delta_t     = self.delta_t.value)
 
             # Scalling output form controller
-            self.twist.omega = self. omega_max * self.twist.omega
+            self.twist.omega = self. omega_max.value * self.twist.omega
             
             # Add header timestamp
             self.twist.header.stamp = rospy.Time.now()
@@ -97,24 +114,21 @@ class WrapperController:
 
         except Exception as e:
             rospy.logerr("Error: {0}".format(e))
-           
-    def cleanup(self):
+
+    def on_shutdown(self):
         # Send stop command
+        self.control_pub.publish(Twist2DStamped(omega=0.0, v=0.0))
+        rospy.sleep(1)
+        self.control_pub.publish(Twist2DStamped(omega=0.0, v=0.0))
+        rospy.sleep(1)
         self.control_pub.publish(Twist2DStamped(omega=0.0, v=0.0))
         # Wait....
         rospy.sleep(1)
         rospy.loginfo("Stop PIDController")
-        pass
+
 
 ###################################################################################
 
-def main():
-        # Create node
-    rospy.init_node("pid_controller_node")
-    node = WrapperController()
-
-    while not rospy.is_shutdown():
-        rospy.spin()
-
-if __name__=="__main__":
-    main()
+if __name__ == '__main__':
+    some_name_node = WrapperController(node_name='pid_controller_node')
+    rospy.spin()
